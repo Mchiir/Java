61bd7ca3c877cfe3fe5554c545daac57428773a0
Mchrispin <mchrispin14@gmail.com>
2024-11-11 20:53:43 +0200

    changed-to-greetme-service

--- src/main/java/com/example/rest_service/Hello.java
+++ src/main/java/com/example/rest_service/Hello.java
@@ -2,11 +2,17 @@ package com.example.rest_service;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
 
 @RestController
+@RequestMapping("/greetme")
 public class Hello {
-    @GetMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
+    @GetMapping(value="/{name}", produces = MediaType.TEXT_HTML_VALUE)
+    public String index(@PathVariable("name") String name) {
+        return "<h1>Greetings "+ name + ", welcome to Springboot services!</h1>";
     }
 }
